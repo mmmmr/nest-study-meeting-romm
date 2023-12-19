4d169ae9207747bf2a42ab2d01d424b5858b0542
mmmmr <ob12564@163.com>
2023-12-19 22:17:58 +0800

    Add login and permission guards

--- sever/src/app.controller.ts
+++ sever/src/app.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, SetMetadata } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RequireLogin, RequirePermission } from './custom.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @RequireLogin()
+  @RequirePermission('ddd')
   getHello(): string {
     return this.appService.getHello();
   }
--- sever/src/permission.guard.ts
+++ sever/src/permission.guard.ts
@@ -1,11 +1,49 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
+
+  @Inject()
+  private readonly reflector: Reflector;
+
+  @Inject()
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+
+    const request: Request = context.switchToHttp().getRequest();
+
+    if(!request.user) {
+      return true;
+    }
+    
+    const permissionCode = this.reflector.getAllAndOverride<string[]>('permission', [
+      context.getClass(),
+      context.getHandler()
+    ])
+    console.log(permissionCode);
+    
+
+    if (!permissionCode) {
+      return true;
+    }
+    
+
+    const userPermissions = request.user.permissions.map(p => p.code)
+
+    const flag = userPermissions.find(item => {
+      return permissionCode.find(it => it === item)
+    })
+
+    if (!flag) {
+      throw new UnauthorizedException('您没有访问该接口的权限');
+    }
+    
+    return true
+
   }
 }
